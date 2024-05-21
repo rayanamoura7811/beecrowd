cf0a19e71b6bb58b13ad1810dd489f149cf90efb
rayanamoura7811 <mourarayanacontato@gmail.com>
2024-05-21 19:37:40 -0300

    Update 1010-calculosimples.py

--- 1010-calculosimples.py
+++ 1010-calculosimples.py
@@ -1,7 +1,13 @@
-vendedor = input()
-salario = float(input())
-vendas = float(input())
+values = input().split(' ')
+codigo1 = int(values[0])
+quantidade1 = int(values[1])
+valor1 = float(values[2])
 
-total = salario + 0.15 * vendas
+values = input().split(' ')
+codigo2 = int(values[0])
+quantidade2 = int(values[1])
+valor2 = float(values[2])
 
-print(f"TOTAL = R$ {total:.2f}")
+total = quantidade1 * valor1 + quantidade2 * valor2
+
+print(f"VALOR A PAGAR: R$ {total:.2f}")
